5e32a9a50ca5b5823b2bdee451a151243f03d97b
naomi-ho <151114635+naomi-ho@users.noreply.github.com>
2024-10-03 23:41:25 -0400

    Refactor functions with classes

--- script.js
+++ script.js
@@ -1,97 +1,115 @@
-const myLibrary = [];
+const titleInput = document.getElementById('title');
+const authorInput = document.getElementById('author');
+const read = document.getElementById('read');
+const container = document.querySelector('.container');
+const form = document.getElementById('form');
 
-const titleInput = document.getElementById("title");
-const authorInput = document.getElementById("author");
-const read = document.getElementById("read");
-const container = document.querySelector(".container");
-
-function Book(title, author, read) {
+class Book {
+  constructor(title, author, read) {
     this.title = title;
     this.author = author;
     this.read = read;
+  }
 }
 
-function addBookToLibrary() {
+class Library {
+  constructor() {
+    this.books = [];
+  }
+
+  addBookToLibrary() {
     // prevents user from submitting empty forms
-    if (titleInput.value == "" || authorInput == "" || read.value == "") {
-        return false;
+    if (titleInput.value == '' || authorInput == '' || read.value == '') {
+      return false;
     }
 
-    let newBookObj = new Book();
+    const newBookObj = new Book(
+      titleInput.value,
+      authorInput.value,
+      read.value,
+    );
 
-    newBookObj.title = titleInput.value;
-    newBookObj.author = authorInput.value;
-    newBookObj.read = read.value;
+    this.books.push(newBookObj);
+    this.displayBooks();
+  }
 
-    myLibrary.push(newBookObj);
-    displayBooks(myLibrary);
-}
-
-function displayBooks() {
-    container.innerHTML = "";
-
-    for (let i = 0; i < myLibrary.length; i++) {
-        const card = document.createElement("div");
-        const title = document.createElement("p");
-        const author = document.createElement("p");
-        const read = document.createElement("p");
-        const btns = document.createElement("div");
-        const removeBtn = document.createElement("button");
-        const statusBtn = document.createElement("button");
-        
-        card.className = "card";
-        title.className = "title";
-        author.className = "author";
-        read.className = "read";
-        btns.className = "btns";
-        removeBtn.className = "removeBtn";
-        statusBtn.className = "statusBtn";
-
-        title.textContent = myLibrary[i].title;
-        author.textContent = "by  " + myLibrary[i].author;
-        statusBtn.textContent = myLibrary[i].read;
-        removeBtn.textContent = "Delete";
-
-        if (statusBtn.textContent === "Complete") {
-            statusBtn.style.backgroundColor = "#8fcc85";
-        } else {
-            statusBtn.style.backgroundColor = "#cc8585";
-        }
-
-        // pass the index of the book to be removed into removeBook()
-        removeBtn.addEventListener("click", function() {
-            container.removeChild(card);
-            removeBook(i);
-        });
-
-        statusBtn.addEventListener("click", function() {
-            changeStatus(i);
-        });
-
-        card.appendChild(title);
-        card.appendChild(author);
-        card.appendChild(read);
-        btns.appendChild(statusBtn);
-        btns.appendChild(removeBtn);
-        card.appendChild(btns);
-        container.appendChild(card);
-    }
-}
-
-// index specifies the index of the book to be removed
-function removeBook(index) {
+  // index specifies the index of the book to be removed
+  removeBook(index) {
     // removes book from array (index at which to start modifying the array, number of elements to remove)
-    myLibrary.splice(index, 1);
+    this.books.splice(index, 1);
     // update the displayed books after removing the book
-    displayBooks();
+    this.displayBooks();
+  }
+
+  // toggles between book completion status
+  changeStatus(index) {
+    if (this.books[index].read === 'Complete') {
+      this.books[index].read = 'Incomplete';
+    } else {
+      this.books[index].read = 'Complete';
+    }
+    this.displayBooks();
+  }
+
+  displayBooks() {
+    container.innerHTML = '';
+
+    for (let i = 0; i < this.books.length; i++) {
+      const card = document.createElement('div');
+      const title = document.createElement('p');
+      const author = document.createElement('p');
+      const read = document.createElement('p');
+      const btns = document.createElement('div');
+      const removeBtn = document.createElement('button');
+      const statusBtn = document.createElement('button');
+
+      card.className = 'card';
+      title.className = 'title';
+      author.className = 'author';
+      read.className = 'read';
+      btns.className = 'btns';
+      removeBtn.className = 'removeBtn';
+      statusBtn.className = 'statusBtn';
+
+      title.textContent = this.books[i].title;
+      author.textContent = 'by  ' + this.books[i].author;
+      statusBtn.textContent = this.books[i].read;
+      removeBtn.textContent = 'Delete';
+
+      if (statusBtn.textContent === 'Complete') {
+        statusBtn.style.backgroundColor = '#8fcc85';
+      } else {
+        statusBtn.style.backgroundColor = '#cc8585';
+      }
+
+      // pass the index of the book to be removed into removeBook()
+      // use arrow functions to maintain correct context
+      removeBtn.addEventListener('click', (e) => {
+        container.removeChild(card);
+        this.removeBook(i);
+      });
+
+      statusBtn.addEventListener('click', (e) => {
+        this.changeStatus(i);
+      });
+
+      card.appendChild(title);
+      card.appendChild(author);
+      card.appendChild(read);
+      btns.appendChild(statusBtn);
+      btns.appendChild(removeBtn);
+      card.appendChild(btns);
+      container.appendChild(card);
+    }
+  }
 }
 
-// toggles between book completion status
-function changeStatus(index) {
-    if (myLibrary[index].read === "Complete") {
-        myLibrary[index].read = "Incomplete";
-    } else {
-        myLibrary[index].read = "Complete";
-    }
-    displayBooks();
-}
+// creates an instance of the Library class
+const myLibrary = new Library();
+
+// attach a submit event listener to the entire form so that it ensures all form submissions, regardless of how it's triggered (e.g. by pressing Enter or clicking submit), is intercepted
+form.addEventListener('submit', (e) => {
+  // prevent default form submission behaviour
+  e.preventDefault();
+  myLibrary.addBookToLibrary();
+});
